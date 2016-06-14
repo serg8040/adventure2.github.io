3a156bd89c783edc9fc1196e1b25a24632903ab3
serg8040 <serg8040@gmail.com>
2016-06-14 12:06:13 +0000

    Add files via upload

---
+++ mes.js
@@ -0,0 +1,35 @@
+﻿/* вывод сообщения по заданному адресу */
+
+//include "..\common\advexter.h"
+//#define empty     '>'
+
+function inimes()
+{   
+    jQuery.ajax({
+        async: false,
+        url:  "http://localhost:8888/text.adv",
+        dataType: 'text',
+        success: function (val) {            
+            textAdv = val;            
+        }
+    });  
+}
+
+var textAdv;
+
+function mes(iadr)
+{
+    var buf = "";
+    adr = iadr * 2;
+    if (textAdv[adr].charCodeAt(0) == 0)
+    {
+        adr++;
+    }
+    while (textAdv[adr].charCodeAt(0) != 0)
+    {
+        buf += textAdv[adr];
+        adr++;
+    }
+    
+    text.value += buf;
+}
---
+++ yes.js
@@ -0,0 +1,33 @@
+﻿function yes(x)
+{
+    var x;
+    var c1;
+   
+    rspeak(x);
+
+    for(;;)
+    {
+        fl_yes = 1;
+        //text.focus();
+        fl_yes = 0;
+        console.log("yes");
+       // var c = str_in[0];  раскомментировать эту строчку
+        c1 = 'y';
+        if( c1 == '' )  
+        {
+            yes(22);
+        }
+        if( c1 == 'y' || c1 == 'Y' || c1 == 'д' || c1 == 'Д' )
+        {
+            x = 1;
+            break;
+        }
+        if( c1 == 'n' || c1 == 'N' || c1 == 'н' || c1 == 'Н' )
+        {
+            x = 0;
+            break;
+        }
+        rspeak(40);
+    }
+    return x;
+}
